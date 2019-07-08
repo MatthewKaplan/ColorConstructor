e41ef4e9f3f7b670c88b0ea3c8f9dc20fbffcd82
B3an5 <beantheo@gmail.com>
2019-07-08 14:09:29 -0600

    create random color and save into state
    Co-authored-by: Matthew Kaplan <kaplan.matthew.p@gmail.com>

--- src/components/PaletteMaker/PaletteMaker.js
+++ src/components/PaletteMaker/PaletteMaker.js
@@ -1,18 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class PaletteMaker extends Component {
-  componentDidMount(){
-    renderRandom
+  constructor() {
+    super();
+    this.state = {
+      paletteTitle: "",
+      projectTitle: "",
+      colors: [
+        { isLocked: false, hex: "" },
+        { isLocked: false, hex: "" },
+        { isLocked: false, hex: "" },
+        { isLocked: false, hex: "" },
+        { isLocked: true, hex: "66666" }
+      ]
+    };
   }
 
+  componentDidMount() {
+    // this.renderRandom
+    this.generateColors();
+  }
+
+  generateColors = e => {
+    if (e) {
+      e.preventDefault();
+    }
+    let paletteColors = this.state.colors;
+
+    paletteColors.forEach(palette => {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      if (!palette.isLocked) {
+        palette.hex = randomColor;
+      }
+    });
+
+    this.setState({
+      colors: paletteColors
+    });
+  };
+
+  colorSave = () => {
+    //post with args being equal to
+  };
+
   render() {
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(this.state);
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
     const paletteStyles = {
       backgroundColor: `#${randomColor}`
-    }
-
-    console.log(randomColor);
+    };
 
     return (
       <div className="palette-maker-component">
@@ -20,13 +57,24 @@ class PaletteMaker extends Component {
           <div className="palette-card" style={paletteStyles}>
             <h1>{randomColor}</h1>
           </div>
-          <div className="palette-card"></div>
-          <div className="palette-card"></div>
-          <div className="palette-card"></div>
-          <div className="palette-card"></div>
+          <div className="palette-card" style={paletteStyles} />
+          <div className="palette-card" style={paletteStyles} />
+          <div className="palette-card" style={paletteStyles} />
+          <div className="palette-card" style={paletteStyles} />
         </section>
+        <form>
+          <input type="text" placeholder="palette name" />
+          <select>
+            <option>sample project</option>
+            <option>sample project</option>
+          </select>
+          <button onClick={e => this.generateColors(e)}>
+            generate new palette
+          </button>
+          <button>save palette</button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
