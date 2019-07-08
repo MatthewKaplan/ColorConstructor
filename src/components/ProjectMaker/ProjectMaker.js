e36ebce1185334be35e0efb8e0e23683e42331a2
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-08 10:23:09 -0600

    Initial file structure for project
    Co-authored-by: Theo Bean <beantheo@gmail.com>

---
+++ src/components/ProjectMaker/ProjectMaker.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProjectMaker extends Component {
+  render() {
+    return (
+      <div className="project-maker-component">
+        <h1>ProjectMaker!</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectMaker;
---
+++ src/components/ProjectPalettes/ProjectPalettes.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProjectPalettes extends Component {
+  render() {
+    return (
+      <div className="project-palettes-component">
+        <h1>ProjectPalettes!</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectPalettes;
