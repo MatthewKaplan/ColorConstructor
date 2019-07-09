baee543b939fc4eefb9f9176d211f980f5f3c4b9
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-08 23:17:32 -0600

    Test addProject and addPalette methods

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,50 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
+const projectsLink = "http://localhost:3000/api/v1/projects";
+const palettesLink = "http://localhost:3000/api/v1/palettes";
+
+const mockColors = [
+  {
+    isLocked: false,
+    hex: "25e8c6",
+    id: 1
+  },
+  {
+    isLocked: false,
+    hex: "a6bdad",
+    id: 2
+  },
+  {
+    isLocked: false,
+    hex: "acfcd1",
+    id: 3
+  },
+  {
+    isLocked: false,
+    hex: "2fa05a",
+    id: 4
+  },
+  {
+    isLocked: false,
+    hex: "cf817c",
+    id: 5
+  }
+];
+
+const projectBody = {
+  body: '{"name":"Project Name"}',
+  headers: { "Content-Type": "application/json" },
+  method: "POST"
+};
+
+const paletteBody = {
+  body:
+    '{"name":"Palette Name","color_1":"25e8c6","color_2":"a6bdad","color_3":"acfcd1","color_4":"2fa05a","color_5":"cf817c","project_id":1}',
+  headers: { "Content-Type": "application/json" },
+  method: "POST"
+};
+
 describe("App", () => {
   let wrapper, instance;
 
@@ -15,6 +59,12 @@ describe("App", () => {
         json: () => Promise.resolve(1)
       })
     );
+    wrapper.setState({
+      projects: [],
+      palettes: [],
+      error: "",
+      loading: false
+    });
   });
 
   it("should match the snapshot", () => {
@@ -45,8 +95,13 @@ describe("App", () => {
   });
 
   describe("fetchProjects", () => {
+    it("should set the state of loading equal to true when first invoked", () => {
+      expect(wrapper.state("loading")).toEqual(false);
+      instance.fetchProjects();
+      expect(wrapper.state("loading")).toEqual(true);
+    });
+
     it("should invoke 'fetch' with correct param", () => {
-      const projectsLink = "http://localhost:3000/api/v1/projects";
       instance.fetchProjects();
       expect(fetch).toHaveBeenCalledWith(projectsLink);
     });
@@ -55,6 +110,12 @@ describe("App", () => {
       expect(wrapper.state("projects")).toEqual([]);
       await instance.fetchProjects();
       expect(wrapper.state("projects")).toEqual(1);
+    });
+
+    it("should set the state of loading equal to false after it has finished fetching", async () => {
+      wrapper.setState({ loading: true });
+      await instance.fetchProjects();
+      expect(wrapper.state("loading")).toEqual(false);
     });
 
     it("should throw an error if the response is not ok and save it to state", async () => {
@@ -68,8 +129,13 @@ describe("App", () => {
   });
 
   describe("fetchPalettes", () => {
+    it("should set the state of loading equal to true when first invoked", () => {
+      expect(wrapper.state("loading")).toEqual(false);
+      instance.fetchPalettes();
+      expect(wrapper.state("loading")).toEqual(true);
+    });
+
     it("should invoke 'fetch' with correct param", () => {
-      const palettesLink = "http://localhost:3000/api/v1/palettes";
       instance.fetchPalettes();
       expect(fetch).toHaveBeenCalledWith(palettesLink);
     });
@@ -80,12 +146,72 @@ describe("App", () => {
       expect(wrapper.state("palettes")).toEqual(1);
     });
 
+    it("should set the state of loading equal to false after it has finished fetching", async () => {
+      wrapper.setState({ loading: true });
+      await instance.fetchPalettes();
+      expect(wrapper.state("loading")).toEqual(false);
+    });
+
     it("should throw an error if the response is not ok and save it to state", async () => {
       expect(wrapper.state("error")).toEqual("");
       window.fetch.mockImplementationOnce(() =>
         Promise.reject(new Error("Fetch failed"))
       );
       await instance.fetchPalettes();
+      expect(wrapper.state("error")).toEqual("Fetch failed");
+    });
+  });
+
+  describe("addProject", () => {
+    it("should invoke 'fetch' with the correct params", () => {
+      instance.addProject("Project Name", "Palette Name", mockColors);
+      expect(fetch).toHaveBeenCalledWith(projectsLink, projectBody);
+    });
+
+    it("should set state of projects with the response of the fetch", async () => {
+      expect(wrapper.state("projects")).toEqual([]);
+      await instance.addProject("Project Name", "Palette Name", mockColors);
+      expect(wrapper.state("projects")).toEqual([1]);
+    });
+
+    it("should invoke 'addPalette' with the correct params", async () => {
+      let mockFn = jest.spyOn(instance, "addPalette");
+      await instance.addProject("Project Name", "Palette Name", mockColors);
+      expect(mockFn).toHaveBeenCalledWith(
+        undefined,
+        "Palette Name",
+        mockColors
+      );
+    });
+
+    it("should throw an error if the response is not ok and save it to state", async () => {
+      expect(wrapper.state("error")).toEqual("");
+      window.fetch.mockImplementationOnce(() =>
+        Promise.reject(new Error("Fetch failed"))
+      );
+      await instance.addProject();
+      expect(wrapper.state("error")).toEqual("Fetch failed");
+    });
+  });
+
+  describe("addPalette", () => {
+    it("should invoke 'fetch' with the correct params", () => {
+      instance.addPalette(1, "Palette Name", mockColors);
+      expect(fetch).toHaveBeenCalledWith(palettesLink, paletteBody);
+    });
+
+    it("should set state of palettes with the response of the fetch", async () => {
+      expect(wrapper.state("palettes")).toEqual([]);
+      await instance.addPalette(1, "Palette Name", mockColors);
+      expect(wrapper.state("palettes")).toEqual([1]);
+    });
+
+    it("should throw an error if the response is not ok and save it to state", async () => {
+      expect(wrapper.state("error")).toEqual("");
+      window.fetch.mockImplementationOnce(() =>
+        Promise.reject(new Error("Fetch failed"))
+      );
+      await instance.addPalette(1, "Palette Name", mockColors);
       expect(wrapper.state("error")).toEqual("Fetch failed");
     });
   });
