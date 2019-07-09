adba6281311e33b5b2b1c272bc0b5b512b75735c
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-08 21:47:13 -0600

    Fetch projects and pallets from backend and save to state. Co-authored-by: Theo Bean <beantheo@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,35 @@ import Header from "../Header/Header";
 import PaletteMaker from "../PaletteMaker/PaletteMaker";
 
 class App extends Component {
+  state = { projects: [], palettes: [], error: "", loading: false };
+
+  componentDidMount() {
+    this.fetchProjects();
+    this.fetchPalettes();
+  }
+
+  fetchProjects = async () => {
+    try {
+      const projects = await fetch("http://localhost:3000/api/v1/projects");
+      const response = await projects.json();
+      this.setState({ projects: response });
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  };
+
+  fetchPalettes = async () => {
+    try {
+      const palettes = await fetch("http://localhost:3000/api/v1/palettes");
+      const response = await palettes.json();
+      this.setState({ palettes: response });
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <Header />
