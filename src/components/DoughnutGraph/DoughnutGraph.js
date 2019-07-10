c3ea83add412e3e0ac94c1f4425fe9848160376e
B3an5 <beantheo@gmail.com>
2019-07-10 00:06:08 -0600

    added hover and labels

--- src/components/DoughnutGraph/DoughnutGraph.js
+++ src/components/DoughnutGraph/DoughnutGraph.js
@@ -9,18 +9,26 @@ class DoughnutGraph extends Component {
         {
           data: [90, 90, 90, 90, 90],
           backgroundColor: [
-            colors[0].hex,
-            colors[1].hex,
-            colors[2].hex,
-            colors[3].hex,
-            colors[4].hex
+            colors[0],
+            colors[1],
+            colors[2],
+            colors[3],
+            colors[4]
+          ],
+          hoverBackgroundColor: [
+            colors[0],
+            colors[1],
+            colors[2],
+            colors[3],
+            colors[4]
           ]
         }
-      ]
+      ],
+      labels: [colors[0], colors[1], colors[2], colors[3], colors[4]]
     };
     return (
       <div className="doughnut-component">
-        <Doughnut data={data} className="doughnut-graph"/>
+        <Doughnut data={data} className="doughnut-graph" />
       </div>
     );
   }
